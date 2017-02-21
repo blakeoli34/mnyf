66fe33776aec945ec07a46913bf2f0a5f34d6b0c
blakeoli34 <blakeoliroyer34@gmail.com>
2017-02-20 22:40:08 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>mnyf – home</title>
+	<link rel="stylesheet" href="css/global.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/glyphs.css" type="text/css" media="screen">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+</head>
+<body class="home">
+	<?php include("inc/nav.inc.php");?>
+	<div class="col-12">
+		<img src="" class="max" alt="heres an image">
+	</div>
+	<div class="col-4">
+		<img src="" class="max" alt="heres another image">
+	</div>
+	<div class="col-8">
+		<img src="" class="max" alt="and yet another one">
+	</div>		
+</body>
+</html>	
